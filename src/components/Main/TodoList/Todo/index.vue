a6f6646e4c3d2ffda09d0c1444100c0f3936b371
megacracker <al.syapukov@gmail.com>
2020-04-18 19:15:43 +0400

    use localstorage for todolist, adding TodoList and Todo components

---
+++ src/components/Main/TodoList/Todo/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <li class="todo">
+    <div class="todo__title">
+      <span v-if="todo.title">{{ todo.title }}</span>
+      <span v-else>Unnamed title</span>
+    </div>
+    <div class="todo__title">
+      <span v-if="todo.title">{{ todo.title }}</span>
+      <span v-else>Unnamed title</span>
+    </div>
+    <div class="todo__options">
+      <div class="todo__edit">
+        <Button theme="green" :isMobile="true" @click="editRoute">Edit</Button>
+      </div>
+      <div class="todo__remove">
+        <Button theme="red" :isMobile="true" @click="modalRemove = !modalRemove">Remove</Button>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+import Button from '@/components/base/Button'
+import CheckBox from '@/components/base/CheckBox'
+export default {
+  props: {
+    todo: {
+      type: Object,
+      default: {}
+    }
+  },
+  components: {
+    Button,
+    CheckBox
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.todo {
+  min-height: 100px;
+  padding: 20px;
+  background-color: #ececec;
+  margin-top: 20px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+  
+  &__title {
+    font-size: 20px;
+  }
+
+  &__options {
+    display: flex;
+    margin-top: 20px;
+  }
+  &__remove {
+    margin-left: 10px;
+  }
+}
+@media screen and (max-width: 559px) {
+  .todo {
+    &__options {
+      flex-direction: column;
+    }
+    &__remove {
+      margin-left: 0;
+      margin-top: 10px;
+    }
+  }
+}
+</style>
---
+++ src/components/Main/TodoList/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="todo-list" v-if="todoList">
+    <Todo v-for="(todo, i) in todoList" :key="i" :todo="todo" :index="i" />
+  </ul>
+  <div v-else class="todo-list__empty">
+    <p>You don't have any ToDo yet!</p>
+    <p>Please, create a ToDo :)</p>
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Main/TodoList/Todo'
+export default {
+  props: {
+    todoList: {
+      type: Array,
+      default: []
+    }
+  },
+  components: {
+    Todo
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.todo-list {
+  display: flex;
+  flex-direction: column;
+  &__empty {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    font-size: 22px;
+    line-height: 36px;
+  }
+}
+</style>
